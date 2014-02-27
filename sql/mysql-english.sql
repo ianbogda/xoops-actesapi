b9a4ceb3b50f2b518f7a2cd4452c703dcc00bfc0
ianbogda <ianbogda@gmail.com>
2014-02-27 11:44:33 +0100

    -del sqls files

--- sql/mysql-english.sql
+++
@@ -1,2 +0,0 @@
-# $Id $
-INSERT INTO actesapi_firsttable (actesapi_id, name, address, telephone, email) VALUES(1, 'John Doe', 'Sea Road', '01 00 00 00 00', 'john@doe.info');
--- sql/mysql-french.sql
+++
@@ -1,2 +0,0 @@
-# $Id $
-INSERT INTO actesapi_firsttable (actesapi_id, name, address, telephone, email) VALUES(1, 'Jacques Martin', 'avenue de la mer', '04 00 00 00 00', 'jacques@martin.info');
